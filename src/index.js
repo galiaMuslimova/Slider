d9d50f1bc42cdbdcc1208f1b5306afd5dc084516
Galia Muslimova <galia.musllimova@gmail.com>
2021-11-05 23:12:00 +0500

    modified:   src/index.js 	modified:   src/index.scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import "./index.scss";
 
+function addNode(className) {
+  let newDiv = document.createElement("div");
+  if(className) {
+    $(newDiv).addClass(className);
+  }
+  return newDiv;
+}
+
+
 (function($) {
 	$.fn.slider = function(options) {		
     var config = $.extend({}, {
@@ -7,7 +16,15 @@ import "./index.scss";
     }, options);
 
     function main(e) {
-      e.append("<div class='slider__container'><div class='slider__connect'></div></div>")
+      let handleLeft = addNode('slider__handle_left');
+      let handleRight = addNode('slider__handle_right');
+      let connect = addNode('slider__connect');
+      connect.appendChild(handleLeft);
+      connect.appendChild(handleRight);
+      let container = addNode('slider__container');
+      container.appendChild(connect);
+
+      e[0].appendChild(container)
     }
     this.each(function () { main($(this)); });
     return this;
