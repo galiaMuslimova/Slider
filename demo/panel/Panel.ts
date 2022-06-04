f28b1def01088709ba47e313fed0d9d2a8392220
Galia Muslimova <galia.musllimova@gmail.com>
2022-06-04 11:17:18 +0500

    remove creating bounds in panel #16

--- demo/panel/Panel.ts
+++ demo/panel/Panel.ts
@@ -24,7 +24,6 @@ class Panel implements IPanel {
     this.$panel = $("<div>");
     this.inputs = {};
     this.initPanel(this.options);
-    this.initBounds(this.options);
     this.bindEventListeners();
   }
 
@@ -60,66 +59,12 @@ class Panel implements IPanel {
     this.inputs = inputs;
   }
 
-  private initBounds(options: IOptions): void {
-    Object.entries(options).forEach(([key, value]) => {
-      this.changeBounds(key, value);
-    });
-  }
-
   private bindEventListeners(): void {
     this.$panel.on("submit", Panel.handlePanelFormSubmit);
   }
 
   private changeOptions(options: IOptions): void {
-    Object.entries(options).forEach(([key, value]) => {
-      this.changeBounds(key, value);
-    });
     this.observer.notify("setting", options);
-  }
-
-  private changeBounds(key: string, value: number): void {
-    const range =
-      Number(this.inputs["max"].getValue()) -
-      Number(this.inputs["min"].getValue());
-    switch (key) {
-      case "min":
-        this.inputs["max"].setProp("min", value);
-        this.inputs["step"].setProp("max", range);
-        this.inputs["from"].setProp("min", value);
-        break;
-      case "max":
-        this.inputs["min"].setProp("max", value);
-        this.inputs["step"].setProp("max", range);
-        this.inputs["from"].setProp(
-          "max",
-          this.inputs["range"].getValue() ? this.inputs["to"].getValue() : value
-        );
-        this.inputs["to"].setProp("max", value);
-        break;
-      case "step":
-        this.inputs["from"].setProp("step", value);
-        this.inputs["to"].setProp("step", value);
-        break;
-      case "from":
-        this.inputs["to"].setProp("min", value);
-        break;
-      case "to":
-        this.inputs["from"].setProp("max", value);
-        break;
-      case "range": {
-        const max = value
-          ? this.inputs["to"].getValue()
-          : this.inputs["max"].getValue();
-        this.inputs["to"].setProp("disabled", !value);
-        this.inputs["from"].setProp("max", max);
-        break;
-      }
-      case "vertical":
-      case "tip":
-        break;
-      default:
-        throw new Error("undefined setting");
-    }
   }
 }
 
